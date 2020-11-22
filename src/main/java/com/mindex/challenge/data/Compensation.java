627014db8173b3b5b6a3412a29a3221036f9e3f7
mattlangsenkamp <langsenkamp.4@osu.edu>
2020-11-22 15:10:01 -0500

    created new types

--- src/main/java/com/mindex/challenge/data/Compensation.java
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -1,4 +1,37 @@
 package com.mindex.challenge.data;
 
+import java.time.Instant;
+
 public class Compensation {
+    private Employee employee;
+    // making assumption salary can have cents
+    private double salary;
+    private Instant effectiveDate;
+
+    public Compensation() {
+    }
+
+    public Employee getEmployee() {
+        return this.employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Instant getEffectiveDate() {
+        return this.effectiveDate;
+    }
+
+    public void setEffectiveDate(Instant effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
 }
